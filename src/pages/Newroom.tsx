32ccdf2f053867bff4c2ec9f86ac6ae6900881c5
Alaa Shouman <alaa@Alaas-MacBook-Air.local>
2025-06-24 10:17:46 +0300

    Checkbox and Avatar. - Updated package.json to include new dependencies for Radix UI components. - Implemented form handling for room details, including validation and state management. - Enhanced UI with improved layout and user experience for room management.

--- src/pages/Newroom.tsx
+++
@@ -1,7 +0,0 @@
-const Newroom = () => {
-  return (
-    <div className="p-4">Newroom</div>
-  )
-}
-
-export default Newroom
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,7 +1,7 @@
 export { default as Dashboard } from "./Dashboard";
 export { default as HotelReservationCalendar } from "./HotelScheduler";
-export { default as Rooms } from "./Rooms";
-export { default as Newroom } from "./Newroom";
+export { default as Rooms } from "./Room/Rooms";
+export { default as Newroom } from "./Room/Newroom";
 export { default as NotFound } from "./NotFound";
 export { default as TeamMembers} from "./TeamMembers/TeamMembers";
 export { default as NewTeamMember} from "./TeamMembers/NewTeamMember";
