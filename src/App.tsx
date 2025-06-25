a554cc2e05e8ba034e9edc4c117d4b9ea5204cc2
alaa <alaa.shouman@st.edu.ul.lb>
2025-06-25 11:16:28 +0300

    Add PostHog integration and update environment variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
 import { BrowserRouter as Router } from 'react-router-dom';
 import { RoleProvider } from './context/CASLContext';
 import HomeRoutes from './routes/Home/homeRoutes';
+import { PostHogProvider } from 'posthog-js/react'
 
 function App() {
   return (
+    <PostHogProvider
+      apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY}
+      options={{
+        api_host: "https://us.i.posthog.com",
+      }}
+    >
     <RoleProvider>
       <Router>
         <HomeRoutes />
       </Router>
     </RoleProvider>
+    </PostHogProvider>
   );
 }
 
